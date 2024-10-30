9539bb077c10b43da404b9550b9c865357cf7735
Jefferson <jeffersondevlsilva@gmail.com>
2024-10-29 22:56:32 -0300

    chore: change home router

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,18 @@ app.use(express.json());
 app.use('/api', userRoutes)
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Bem-vindo à API de Agendamentos de Consultas Online! Acesse /status para a mais detalhes da api.');
+});
+// app.get('/api-docs', (req, res) => {
+//     res.sendFile(__dirname + '/path/to/your/documentation.html');
+// });
+
+app.get('/status', (req, res) => {
+    res.json({
+        status: 'OK',
+        uptime: process.uptime(),
+        message: 'API está funcionando corretamente'
+    });
 });
 
 app.listen(PORT, () => {
